41a68e460b850679e6c8864dfcc5a85e72a96f3b
Corax73 <ramaz999@inbox.ru>
2024-05-19 14:35:34 +0400

    added receiving users data

--- index.php
+++ index.php
@@ -105,24 +105,17 @@ require 'src/main.php';
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Larry</td>
-                        <td>the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
+                    <?php if ($calls) { ?>
+                        <?php for ($i = 1; $i < count($calls); $i++) { ?>
+                        <tr>
+                            <th scope="row"><?= $i ?></th>
+                            <td><?= $calls[$i]['user'] ?></td>
+                            <td><?= $calls[$i]['dialed_user'] ?></td>
+                            <td><?= $calls[$i]['call_start_time'] ?></td>
+                            <td><?= $calls[$i]['call_end_time'] ?></td>
+                        </tr>
+                        <?php } ?>
+                    <?php } ?>
                 </tbody>
             </table>
         </div>
--- src/main.php
+++ src/main.php
@@ -1,6 +1,27 @@
 <?php
 
+use Models\Call;
 use Models\User;
+
+$call = new Call();
+$calls = collect($call->all());
+$userIds = $calls->pluck('user_id')->toArray();
+$dialedUserIds = $calls->pluck('dialed_user_id')->toArray();
+$user = new User();
+$users = collect($user->find($userIds));
+$dialedUsers = collect($user->find($dialedUserIds));
+$calls = $calls->map(function ($item) use ($users, $dialedUsers) {
+    $user = $users->where('id', $item['user_id']);
+    if ($user) {
+        $item['user'] = $user->first()['email'];
+    }
+
+    $dialedUser = $dialedUsers->where('id', $item['dialed_user_id']);
+    if ($dialedUser) {
+        $item['dialed_user'] = $dialedUser->first()['email'];
+    }
+    return $item;
+});
 
 if ((isset($_POST['email']) && isset($_POST['passwordForLogin'])) && ($_POST['email'] && $_POST['passwordForLogin'])) {
     $user = new User();
